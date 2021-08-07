8d7d2f5c98bd7ac1364365d9ac40df8f0620a41a
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-08-07 14:14:21 -0300

    angular app + material + cdk

---
+++ apps/creator/desktop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'devparana-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'creator-desktop';
+}
