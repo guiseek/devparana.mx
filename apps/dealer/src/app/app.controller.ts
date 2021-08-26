bfafacaead6010bb66b6fd7a14c3b48694200020
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-08-26 13:19:08 -0300

    feat: nest

---
+++ apps/dealer/src/app/app.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { LocalAuthGuard } from '../auth/local-auth.guard';
+import { AuthService } from '../auth/auth.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { PublicZone } from '../auth/public-zone.decorator';
+
+@Controller()
+export class AppController {
+  constructor(private authService: AuthService) {}
+
+  // @UseGuards(AuthGuard('local'))
+  // @Post('auth/login')
+  // async login(@Request() req) {
+  //   return req.user;
+  // }
+
+  // @UseGuards(LocalAuthGuard)
+  // @Post('auth/login')
+  // async login(@Request() req) {
+  //   return req.user;
+  // }
+
+  @PublicZone()
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+}
